520eb000de05f9f637efccc14208f94d3504f2bb
Jorge <jorge.camara@iesdoctorbalmis.com>
2021-05-17 12:29:20 +0200

    yamete kudasai

--- mijs.js
+++ mijs.js
@@ -1,6 +1,12 @@
 loadDocA("libros.xml","xml");
 
-function gestionarFicheroXML(fichero)
+function gestionarFicheroXML(xmlDoc)
 {
-    alert(fichero);
+    alert(fichero)
+
+	let capaVacia = document.querySelector("#ficheroXML")
+	let libros = xmlDoc.querySelectorAll("libro")
+	for(let i=0; i<libros.length; i++)
+		capaVacia.innerHTML = capaVacia.innerHTML + "<p>" + libros[i].textContent + "</p>"
+	
 }
